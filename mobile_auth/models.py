52016763283adf087bea91b507091388d8a02b7b
Afash <afashseven@gmail.com>
2024-05-04 00:17:45 +0330

    sms model

--- mobile_auth/models.py
+++ mobile_auth/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class UserMobileAuth(models.Model):
     phone_number = models.CharField(max_length=15, unique=True)
+    verification_code = models.CharField(max_length=6)
+    verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
