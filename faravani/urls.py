3d720010cf967eae66f1a4f55efd71fb8e8c42f5
Afash <afashseven@gmail.com>
2024-05-28 11:33:06 +0330

    check release styles

--- faravani/urls.py
+++ faravani/urls.py
@@ -4,8 +4,20 @@ Developer: afash.ir
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("blog/", include("blog.urls")),
+    path('users/', include('users.urls')),
+    path('items/', include('items.urls')),
+    path('communications/', include('communications.urls')),
+    path('verification/', include('verification.urls')),
+    path('uploads/', include('uploads.urls')),
+    path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './static/**/*.css',
+    './styles/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
